228943738294022aa05908f13a1d84186b181815
Rustam Klekit <rustam_klekit@epam.com>
2020-02-14 09:33:53 +0200

    changed code so another test could be added

--- src/main/java/com/epam/edp/demo/RegexReceiver.java
+++ src/main/java/com/epam/edp/demo/RegexReceiver.java
@@ -3,16 +3,21 @@ package com.epam.edp.demo;
 import java.util.regex.Pattern;
 
 public class RegexReceiver {
-	
-	private static final Pattern EMAIL_PATTERN = Pattern.compile("@gmail");	//change string inside the parentheses
-	
-	
-	static boolean isEmail(String s) {
-		return EMAIL_PATTERN.matcher(s).matches();
-	}
-	
-	public static void main(String [] args) {
-		//you can test your code here before sending it to the review
-	}
+
+    public static final String EMAIL_PATTERN_STRING = "@gmail"; //change string inside the parentheses
+
+    private static Pattern emailPattern;
+
+
+    static boolean isEmail(String s) {
+        if (emailPattern == null) {
+            emailPattern = Pattern.compile(EMAIL_PATTERN_STRING);
+        }
+        return emailPattern.matcher(s).matches();
+    }
+
+    public static void main(String[] args) {
+        //you can test your code here before sending it to the review
+    }
 
 }
