6e4b5653fe3220fce68f80105cdea789ab3d0078
Autocode <autocode.test@yandex.ru>
2020-04-17 16:31:49 +0300

    web address regex

--- src/main/java/com/epam/edp/demo/RegexReceiver.java
+++ src/main/java/com/epam/edp/demo/RegexReceiver.java
@@ -5,8 +5,10 @@ import java.util.regex.Pattern;
 public class RegexReceiver {
 
     public static final String EMAIL_PATTERN_STRING = "@gmail"; //change string inside the parentheses
+    public static final String WEB_ADDRESS_PATTERN_STRING = "google.com"; //change string inside the parentheses
 
     private static Pattern emailPattern;
+    private static Pattern webAddressPattern;
 
 
     static boolean isEmail(String s) {
@@ -14,6 +16,13 @@ public class RegexReceiver {
             emailPattern = Pattern.compile(EMAIL_PATTERN_STRING);
         }
         return emailPattern.matcher(s).matches();
+    }
+
+    static boolean isWebAddress(String s) {
+        if (webAddressPattern == null) {
+            webAddressPattern = Pattern.compile(WEB_ADDRESS_PATTERN_STRING);
+        }
+        return webAddressPattern.matcher(s).matches();
     }
 
     public static void main(String[] args) {
